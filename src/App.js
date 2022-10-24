0768bac0fed2a6ca488a27f50d0726c8d1423d2c
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-10-24 13:34:29 +0200

    started the frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './index.css';
-function App() {
+import { useState } from 'react';
+import { BsArrowLeftCircleFill } from 'react-icons/bs';
+import personal from './assets/personal.jpg'
+
+const App = () => {
+  const [open, setOpen] = useState(true)
+
   return (
-    <div >
-     <h1 className='bg-red-300'>Portfolio</h1>
+    <div className='flex'>
+      <div className={`${open ? 'w-72' : 'w-20'} duration-200 h-screen bg-dark-purple relative`}>
+        <BsArrowLeftCircleFill alt='close and open icon' className={`absolute cursor-pointer rounded-full -right-3 top-9 w-10 h-10 bg-slate-500 border-2 border-dark-purple ${!open && 'rotate-180'}`} onClick={()=>setOpen(!open)}/>
+        <div className='flex gap-3 items-center'>
+          <img src={personal} alt='personal' className='cursor-pointer duration-500 rounded-full h-20' />
+          <h2 className={`text-white origin-left font-medium text-x1 duration-300`}>Samuel Nzekwe</h2>
+        </div>
+      </div>
+      <div className='p-7 text-2xl font-semibold flex-1 h-screen'>
+        <h1>Home page</h1>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "dark-purple": "#081A51",
+        "light-white": "rgba(255, 255, 255,0.17)"
+      },
+    },
   },
   plugins: [],
 }
+
+
