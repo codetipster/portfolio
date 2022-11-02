ca4206394494c653f066b7d369c145aa44cac16a
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-11-02 10:18:05 +0100

    routing for header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './index.css';
 import Header from './components/Header';
 import Login from '../src/pages/Login'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
+    <BrowserRouter>
     <Header>
-      <Login/>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </Header>
+    </BrowserRouter>
   )
 }
   
--- src/components/Header.js
+++ src/components/Header.js
@@ -249,7 +249,7 @@ const Header = (props) => {
             </Popover>
           </Popover.Group>
           <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-            <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="/login" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
               Sign in
             </a>
             <a
